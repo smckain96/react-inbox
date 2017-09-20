c12df3cdf4dbf84d7ee3cbc75b03d1ec7d681dce
smckain96 <alexjmckain@gmail.com>
2017-09-20 13:59:06 -0700

    fixing Message component

--- src/Message.js
+++ src/Message.js
@@ -3,6 +3,7 @@ import './App.css';
 import Messages from './Messages'
 
 const Message = (props) => {
+  console.log(props)
   let starHandler = () => {
     props.handleStar(props.email.id)
   }
@@ -20,35 +21,23 @@ const Message = (props) => {
   })
 
   return (
-  //     <div className="col-xs-2">
-  //       <i className={this.props.email.starred ? "star fa fa-star" : "star-fa fa-star-o"} onClick={this.props.handleStar(this.id)}></i>
-  //     </div>
-  //   </div>
-  // </div>
-  // <div className="col-xs-11">
-  // <span className="label label-warning">dev</span>    <a href="#">
-  //     {this.props.emailsubject}
-  //   </a>
-  // </div>
-// </div>
-        // </div>
-<div className={props.email.selected ? "row message selected" : props.email.read ? "row message read" : "row message unread"}>
-  <div className="col-xs-1">
-    <div className="row">
-      <div className="col-xs-2" checked={props.handleCheckbox(props.email.id)}>
-        {props.email.selected ? <input type="checkbox" checked/> : <input type="checkbox"/>}
+    <div className={props.email.selected ? "row message selected" : props.email.read ? "row message read" : "row message unread"}>
+      <div className="col-xs-1">
+        <div className="row">
+          <div className="col-xs-2" checked={props.handleCheckbox(props.email.id)}>
+            {props.email.selected ? <input type="checkbox" checked/> : <input type="checkbox"/>}
+          </div>
+          <div className="col-xs-2">
+            <i className={props.email.starred ? "star fa fa-star" : "star-fa fa-star-o"} onClick={props.handleStar(props.email.id)}></i>
+          </div>
+        </div>
       </div>
-      <div className="col-xs-2">
-        // <i className={props.email.starred ? "star fa fa-star" : "star-fa fa-star-o"} onClick={props.handleStar(props.email.id)}></i>
+      <div className="col-xs-11">
+        <a href="#">
+          {props.email.subject}
+        </a>
       </div>
     </div>
-  </div>
-  <div className="col-xs-11">
-    <a href="#">
-      {props.email.subject}
-    </a>
-  </div>
-</div>
   );
 }
 
